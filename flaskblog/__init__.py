8d5b0d037044054bf3e8de55d6d3e3934bb23840
longhakly <long.hakly19@kit.edu.kh>
2021-07-23 17:07:44 +0700

    pagination

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'da3d570c388f2383ed557e0f28d99470'
@@ -11,5 +12,5 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
+app.config['MAIL_SERVER'] 
 from flaskblog import routes
