b117d9c23ddbac72e4dcf6be73e2438f4e2e1a63
longhakly <long.hakly19@kit.edu.kh>
2021-07-23 18:16:57 +0700

    create token and reset password by send email with token

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -12,5 +13,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yerngtest@gmail.com'
+app.config['MAIL_PASSWORD'] = 'S45665400s'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
 from flaskblog import routes
